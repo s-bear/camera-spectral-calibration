5fcb75ccff4f6e4b5d96826f1e9cbbda939cb0f5
Samuel Bear Powell <>
2023-01-09 14:42:25 +1000

    source links include line ranges

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,11 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import os,sys
+import os,sys, importlib, inspect, warnings
+import subprocess
+
+from sphinx.ext.autodoc.mock import mock
+
 sys.path.insert(0, os.path.abspath('../scripts'))
 
 # -- Project information -----------------------------------------------------
@@ -13,6 +17,8 @@ project = 'Camera Spectral Calibration'
 copyright = '2022, Samuel Powell'
 author = 'Samuel Powell'
 release = '2022-12-05'
+
+source_repository = 'https://github.com/s-bear/camera-spectral-calibration'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -37,18 +43,51 @@ autodoc_member_order = 'bysource'
 autodoc_type_aliases = {'NDArray': 'numpy.ndarray', 'ArrayLike': 'array_like'}
 autodoc_mock_imports = ['numpy','numba','scipy','matplotlib','h5py','sklearn','imageio','piexif','xlsxwriter']
 
-source_repository = 'https://github.com/s-bear/camera-spectral-calibration'
-source_directory = 'scripts'
+def get_commit():
+    # use git to get the commit hash
+    cmd = 'git describe --dirty --tags --long --always'.split(' ')
+    commit = subprocess.run(cmd,capture_output=True,check=True,text=True,timeout=1).stdout.strip()
+    if 'dirty' in commit:
+        warnings.warn('Generating docs on dirty repository -- source links will be broken!')
+    return commit
+
+commit = get_commit()
+
+source_url = f'{source_repository}/blob/{commit}'
 
 def linkcode_resolve(domain, info):
-    if domain != 'py':
-        return None
-    if 'module' not in info:
-        return None
-    fname = info['module'].replace('.','/')
-    return f'{source_repository}/blob/main/{source_directory}/{fname}.py'
+    #modified from https://github.com/aaugustin/websockets/blob/main/docs/conf.py
 
-# for a fancy linkcode_resolve see https://github.com/aaugustin/websockets/blob/778a1ca6936ac67e7a3fe1bbe585db2eafeaa515/docs/conf.py#L100-L134
+    assert domain == "py", "expected only Python objects"
+
+    with mock(autodoc_mock_imports):
+        mod = importlib.import_module(info["module"])
+
+    if "." in info["fullname"]:
+        objname, attrname = info["fullname"].split(".")
+        obj = getattr(mod, objname)
+        try:
+            # object is a method of a class
+            obj = getattr(obj, attrname)
+        except AttributeError:
+            # object is an attribute of a class
+            return None
+    else:
+        obj = getattr(mod, info["fullname"])
+
+    try:
+        file = inspect.getsourcefile(obj)
+        lines = inspect.getsourcelines(obj)
+    except TypeError:
+        # e.g. object is a typing.Union
+        return None
+    file = os.path.relpath(file, os.path.abspath("."))
+    if not file.startswith("scripts"):
+        # e.g. object is a typing.NewType
+        return None
+    start, end = lines[1], lines[1] + len(lines[0]) - 1
+
+    return f"{source_url}/{file}#L{start}-L{end}"
 
 
 
